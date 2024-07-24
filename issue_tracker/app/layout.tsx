fb78076b8f52df7fe4058a0d742c31d2a2291111
SanduniIshara <sanduniishara1@outlook.com>
2024-07-24 15:13:40 +0530

    5th commit

--- issue_tracker/app/layout.tsx
+++ issue_tracker/app/layout.tsx
@@ -1,6 +1,9 @@
+import '@radix-ui/themes/styles.css';
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import NavBar from './NavBar';
+import { Theme,ThemePanel } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>
+         <NavBar/>
+         <main className='p-5'>{children}</main>
+         <ThemePanel/>
+        </Theme>
+      </body>
     </html>
   )
 }
